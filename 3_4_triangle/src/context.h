b2878294dcf4fb6ed74eacc4b0b0639e7b2c6ef0
Minseok Kim <minseokkim@Minseokui-MacBookPro.local>
2022-06-21 11:51:26 +0900

    first commit

---
+++ 3_4_triangle/src/context.h
@@ -0,0 +1,23 @@
+#ifndef __CONTEXT_H__
+#define __CONTEXT_H__
+
+#include "common.h"
+#include "shader.h"
+#include "program.h"
+
+CLASS_PTR(Context)
+class Context {
+public:
+    static ContextUPtr Create();
+    void Render();    
+private:
+    Context() {}
+    bool Init();
+    ProgramUPtr m_program;
+
+    uint32_t m_vertexBuffer;
+    uint32_t m_vertexArrayObject;
+};
+
+#endif // __CONTEXT_H__
+
